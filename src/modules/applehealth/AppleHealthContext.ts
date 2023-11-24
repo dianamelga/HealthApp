4b49a31691aa8bbe5e675e6cf320f0cab293898e
Diana <di.melgarejo@gmail.com>
2023-11-24 08:33:56 -0300

    initial commit

---
+++ src/modules/applehealth/AppleHealthContext.ts
@@ -0,0 +1,8 @@
+import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+
+export const AppleHealthContextProvider: React.FC<Props> = ({children}) => {
+  const dispatch = useDispatch();
+};
