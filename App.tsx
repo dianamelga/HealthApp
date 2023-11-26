2958e36071e22f754ea01ed2e35a91b6a1566e83
Diana <di.melgarejo@gmail.com>
2023-11-26 12:18:55 -0300

    add react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
+import {PaperProvider, useTheme} from 'react-native-paper';
 
 import {AppleHealthProvider} from './src/modules/applehealth/AppleHealthContext';
 import AppleHealthScreen from './src/screens/AppleHealthScreen';
 
 export default function App() {
+  const theme = useTheme();
   return (
     <>
       <AppleHealthProvider>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <AppleHealthScreen />
-        </SafeAreaView>
+        <PaperProvider theme={theme}>
+          <StatusBar barStyle="dark-content" />
+          <SafeAreaView>
+            <AppleHealthScreen />
+          </SafeAreaView>
+        </PaperProvider>
       </AppleHealthProvider>
     </>
   );
