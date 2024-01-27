595cdd0ff3c5810eecd7a21642caece2a97abb21
Diana <di.melgarejo@gmail.com>
2024-01-27 20:00:35 -0300

    modify styles

--- src/screens/apple-health/AppleHealthScreen.styled.ts
+++ src/screens/apple-health/AppleHealthScreen.styled.ts
@@ -4,14 +4,12 @@ import {DefaultTheme, Text} from 'react-native-paper';
 const Styled = {
   Body: styled.View`
     background-color: ${DefaultTheme.colors.background};
-    border-width: 1px;
     justify-content: center;
   `,
   Container: styled.View`
     margin-top: 32px;
     padding-horizontal: 24px;
     border-color: red;
-    border-width: 1px;
   `,
   Title: styled(Text)`
     color: ${DefaultTheme.colors.primary};
